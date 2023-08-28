ed5c29f38da593d32cee58040a01d80dfb734617
Klement <lucasklementr@gmail.com>
2023-08-28 19:49:24 -0300

    Made compile compatible with c99

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+rm bin/server bin/client -f
 cd src
-gcc -I../include server.c udp_server.c udp_common.c simulation.c supervisory.c timer.c -lpthread -lSDL -lm -std=c11 -o ../bin/server
-gcc -I../include client.c udp_client.c udp_common.c controller.c supervisory.c timer.c -lpthread -lSDL -lm -std=c11 -o ../bin/client
+gcc -I../include server.c udp_server.c udp_common.c simulation.c supervisory.c timer.c -lpthread -lSDL -lm -lrt -std=c99 -o ../bin/server
+gcc -I../include client.c udp_client.c udp_common.c controller.c supervisory.c timer.c -lpthread -lSDL -lm -lrt -std=c99 -o ../bin/client
